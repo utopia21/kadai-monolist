4ea0d1f873c8903fb25856400447a7498d933532
yuto.hommura <utopia21.cw@gmial.com>
2018-07-27 05:45:09 +0000

    want

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,4 +7,21 @@ class User < ApplicationRecord
                     uniqueness: { case_sensitive: false }
 
   has_secure_password
+  
+  has_many :ownerships
+  has_many :items, through: :ownerships
+  has_many :wants
+  has_many :want_items, through: :wants, class_name: "Item", source: :item
+  
+  def want(item)
+    wants.find_or_create_by(item_id: item.id)
+  end
+  
+  def unwant(item)
+    wants.find_by(item_id: item.id).destroy
+  end
+  
+  def want?(item)
+    want_items.include?(item)
+  end
 end
